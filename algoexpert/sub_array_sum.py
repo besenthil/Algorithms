6e58bd1ef5b62bbe84d07d0e0e08aa2205e2c58f
Senthilkumar Balasubramanian <besenthil@gmail.com>
2022-01-19 07:58:19 +0530

    Day 7 code

---
+++ algoexpert/sub_array_sum.py
@@ -0,0 +1,11 @@
+# o(n) - time
+# o(1) - space
+def kadanesAlgorithm(array):
+    max_so_far = array[0]
+    max_here = array[0]
+    for idx in range(1, len(array)):
+        if max_here < 0:
+            max_here = 0
+        max_here += array[idx]
+        max_so_far = max(max_so_far, max_here)
+    return max_so_far
