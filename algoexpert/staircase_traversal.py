69ca084805fd23314ec046ec65c296da0d91e393
Senthilkumar Balasubramanian <besenthil@gmail.com>
2022-02-13 05:50:15 +0530

    Day 32 code

--- algoexpert/staircase_traversal.py
+++ algoexpert/staircase_traversal.py
@@ -11,3 +11,20 @@ def staircaseTraversal_helper(height, current_height, steps):
         if (current_height + step) <= height:
             total_ways += staircaseTraversal_helper(height, current_height + step, steps)
     return total_ways
+
+
+def staircaseTraversal_memoization(height, maxSteps):
+    # Write your code here.
+    return numberOfWaysToTop_memoized(height, maxSteps, {0: 1, 1: 1})
+
+
+def numberOfWaysToTop_memoized(height, maxSteps, memoized):
+    if height in memoized:
+        return memoized[height]
+    number_of_ways = 0
+    for step in range(1, min(maxSteps, height) + 1):
+        number_of_ways += numberOfWaysToTop_memoized(height - step, maxSteps, memoized)
+
+    memoized[height] = number_of_ways
+
+    return number_of_ways
